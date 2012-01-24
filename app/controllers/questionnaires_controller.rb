b8fad49fcaa426e9fb0aa6f37f34319e90502919
Larry Scott <iamjustfresh@gmail.com>
2012-01-24 04:06:43 -0500

    made change

--- app/controllers/questionnaires_controller.rb
+++ app/controllers/questionnaires_controller.rb
@@ -1,11 +1,11 @@
 class QuestionnairesController < ApplicationController
-  def index
-    @questionnaires = Questionnaire.all
-  end
-
-  def show
-    @questionnaire = Questionnaire.find(params[:id])
-  end
+  # def index
+  #   @questionnaires = Questionnaire.all
+  # end
+  # 
+  # def show
+  #   @questionnaire = Questionnaire.find(params[:id])
+  # end
 
   def new
     @questionnaire = Questionnaire.new
@@ -14,28 +14,29 @@ class QuestionnairesController < ApplicationController
   def create
     @questionnaire = Questionnaire.new(params[:questionnaire])
     if @questionnaire.save
-      redirect_to @questionnaire, :notice => "Successfully created questionnaire."
+      redirect_to root_url, :notice => "Successfully created questionnaire."
     else
       render :action => 'new'
     end
+    # redirect_to root_url
   end
 
-  def edit
-    @questionnaire = Questionnaire.find(params[:id])
-  end
+  # def edit
+  #    @questionnaire = Questionnaire.find(params[:id])
+  #  end
 
-  def update
-    @questionnaire = Questionnaire.find(params[:id])
-    if @questionnaire.update_attributes(params[:questionnaire])
-      redirect_to @questionnaire, :notice  => "Successfully updated questionnaire."
-    else
-      render :action => 'edit'
-    end
-  end
-
-  def destroy
-    @questionnaire = Questionnaire.find(params[:id])
-    @questionnaire.destroy
-    redirect_to questionnaires_url, :notice => "Successfully destroyed questionnaire."
-  end
+  # def update
+  #   @questionnaire = Questionnaire.find(params[:id])
+  #   if @questionnaire.update_attributes(params[:questionnaire])
+  #     redirect_to @questionnaire, :notice  => "Successfully updated questionnaire."
+  #   else
+  #     render :action => 'edit'
+  #   end
+  # end
+  # 
+  # def destroy
+  #   @questionnaire = Questionnaire.find(params[:id])
+  #   @questionnaire.destroy
+  #   redirect_to questionnaires_url, :notice => "Successfully destroyed questionnaire."
+  # end
 end
